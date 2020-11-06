f1b3d310dc33b211593577c4b8219ae10f808af6
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-06 13:42:44 -0700

    add joke div

--- src/Components/JokeDiv/JokeDiv.js
+++ src/Components/JokeDiv/JokeDiv.js
@@ -4,10 +4,10 @@ import './Joke-Div.scss'
 export default function JokeDiv(props) {
     return (
         <section className='joke-section'>
-            {props.checkForJoke()}
             <div>
                 <h1>👍</h1>
                 <h1>👎</h1>
+                <div>{props.checkForJoke()}</div>
             </div>
         </section>
     )
