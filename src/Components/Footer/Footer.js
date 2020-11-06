3e11836fecebfc2944b18fd259478073e5989963
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-05 23:24:52 -0700

    style footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import './Footer.scss'
 
 export default function Footer() {
     return (
-        <div>
-            <h1>Chris</h1>
+        <div className='footer'>
+            <h1 className='title'>Chris</h1>
         </div>
     )
 }
