c2ff7d86a1eae45cfe2e96eab9cb22dee35dcf34
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-05 19:56:16 -0700

    Create Footer Structure

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,25 +1,9 @@
 import React, { Component } from 'react'
 
-export default class Footer extends Component {
-    constructor() {
-        super()
-        this.state = {
-            joke: {}
-        }
-    }
-    checkForJoke = () => {
-        if (this.state.joke.value) {
-            return this.state.joke.value.joke
-        } else {
-            return 'Loading'
-            // {this.checkForJoke()}
-        }
-    }
-    render() {
-        return (
-            <div>
-                <h1></h1>
-            </div>
-        )
-    }
+export default function Footer() {
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    )
 }
