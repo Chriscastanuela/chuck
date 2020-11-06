3f7910148ed40f8ac938cf5430e826ce5e282d17
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-05 19:59:14 -0700

    Turn App into a class

--- src/App.js
+++ src/App.js
@@ -2,26 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './Components/Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Footer></Footer>
-    </div>
-  );
+export default class App {
+  constructor() {
+    super()
+    this.state = {
+      joke: {}
+    }
+  }
+  checkForJoke = () => {
+    if (this.state.joke.value) {
+      return this.state.joke.value.joke
+    } else {
+      return 'Loading'
+      // {this.checkForJoke()}
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Footer></Footer>
+      </div>
+    );
+  }
 }
 
 export default App;
