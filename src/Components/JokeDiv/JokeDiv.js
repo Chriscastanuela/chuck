4249ae1ec2d4bdb3c95c50bcdc5e28bfbd127173
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-06 21:06:23 -0700

    change font size

--- src/Components/JokeDiv/JokeDiv.js
+++ src/Components/JokeDiv/JokeDiv.js
@@ -5,15 +5,9 @@ export default function JokeDiv(props) {
     return (
         <section className='joke-section'>
             <div className='joke-div'>
-                {/* <div className='thumbs'>
-                    <h1 className='thumb'>👍</h1>
-                    <h1 className='thumb'>👎</h1>
-                </div>
-                <div>{props.checkForJoke()}</div> */}
                 <h1 className='thumbs' id='left'>👍</h1>
                 <h1 className='thumbs' id='right'>👎</h1>
                 <p className='the-joke'>{props.checkForJoke()}</p>
-                {/* <div className='joke'>{props.checkForJoke()}</div> */}
             </div>
         </section>
     )
