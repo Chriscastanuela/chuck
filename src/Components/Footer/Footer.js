6ef16249c77bed7735fb370af8e48d767cbffc70
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-08 14:45:17 -0700

    footer - footer.js - add imports

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,5 @@
-// import React, { Component } from 'react'
-// import './Footer.scss'
+import React, { Component } from 'react'
+import './Footer.scss'
 
 // export default function Footer() {
 //     return (
