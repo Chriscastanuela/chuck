e782fdeb82dc7d8e7a552f430c776c0f13bec3e2
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-08 14:45:38 -0700

    footer - footer.js - add return()

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import './Footer.scss'
 
-// export default function Footer() {
-//     return (
-//         <div className='footer-body'>
-//             <a 
-//             href='https://chriscastanuela.github.io/Christopher-Anthony-Castanuela/'
-//             className='footer-link'
-//             ><input type="button" className='footer-title' value="Author"/></a>
-//         </div>
-//     )
-// }
+export default function Footer() {
+    return (
+        <div className='footer-body'>
+            <a 
+            href='https://chriscastanuela.github.io/Christopher-Anthony-Castanuela/'
+            className='footer-link'
+            ><input type="button" className='footer-title' value="Author"/></a>
+        </div>
+    )
+}
