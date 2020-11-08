2555885a77207e0a0cc212575f4735eeb944d2de
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-08 15:00:18 -0700

    Main - Main.js - add return()

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -3,28 +3,28 @@ import './Main.scss';
 import JokeDiv from '../JokeDiv/JokeDiv';
 import loading from '../../Assets/loading.png';
 
-// export default function Main(props) {
-//     if (props.jokes.length > 0) {
-//         let sortedJokes = props.jokes[0].value.sort((a, b) => {
-//             return a.joke.localeCompare(b.joke)
-//         })
-//         return (
-//             <div className='main-section'>
-//                 {
-//                     sortedJokes.map(element => {
-//                         return (
-//                             <JokeDiv 
-//                             decodeHtml={props.decodeHtml}
-//                             theJoke={element.joke}
-//                             like={props.like}
-//                             isMain={props.isMain}
-//                             />
-//                         )
-//                     })
-//                 }
-//             </div>
-//         )
-//     } else {
-//         return <img src={loading} alt="loading image" className='loading'/>
-//     }
-// }
+export default function Main(props) {
+    if (props.jokes.length > 0) {
+        let sortedJokes = props.jokes[0].value.sort((a, b) => {
+            return a.joke.localeCompare(b.joke)
+        })
+        return (
+            <div className='main-section'>
+                {
+                    sortedJokes.map(element => {
+                        return (
+                            <JokeDiv 
+                            decodeHtml={props.decodeHtml}
+                            theJoke={element.joke}
+                            like={props.like}
+                            isMain={props.isMain}
+                            />
+                        )
+                    })
+                }
+            </div>
+        )
+    } else {
+        return <img src={loading} alt="loading image" className='loading'/>
+    }
+}
