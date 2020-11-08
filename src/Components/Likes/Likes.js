8ff99e46850ce232de7d28beb2caf9a02fe5ee8a
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-08 13:38:33 -0700

    change likes to a function

--- src/Components/Likes/Likes.js
+++ src/Components/Likes/Likes.js
@@ -7,35 +7,33 @@ import chuck from '../../Assets/chuck.jpeg';
 import JokeDiv from '../JokeDiv/JokeDiv';
 
 
-export default class Likes extends Component {
-    render(props) {
-        let sortedJokes = this.props.likes.sort((a, b) => {
-            return a.localeCompare(b)
-        });
-        if (this.props.likes.length > 0) {
-            return (
-                <div className='likes-section'>
-                    {
-                        sortedJokes.map(element => {
-                            return (
-                                <JokeDiv 
-                                decodeHtml={this.props.decodeHtml}
-                                theJoke={element}
-                                dislike={this.props.dislike}
-                                isLikes={this.props.isLikes}
-                                />
-                            )
-                        })
-                    }
-                </div>
-            )
-        } else {
-            return  (
-                <div>
-                    <h1 className='likes-header'>You have no likes</h1>
-                    <img src={chuck} alt="loading image" className='chuck'/>
-                </div>
-            )
-        }
+export default function Likes(props) {
+    let sortedJokes = props.likes.sort((a, b) => {
+        return a.localeCompare(b)
+    });
+    if (props.likes.length > 0) {
+        return (
+            <div className='likes-section'>
+                {
+                    sortedJokes.map(element => {
+                        return (
+                            <JokeDiv 
+                            decodeHtml={props.decodeHtml}
+                            theJoke={element}
+                            dislike={props.dislike}
+                            isLikes={props.isLikes}
+                            />
+                        )
+                    })
+                }
+            </div>
+        )
+    } else {
+        return  (
+            <div>
+                <h1 className='likes-header'>You have no likes</h1>
+                <img src={chuck} alt="loading image" className='chuck'/>
+            </div>
+        )
     }
 }
