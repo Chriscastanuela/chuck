3b5957433e4cf03de4e84cd34afd1016f33ea622
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-08 20:23:17 -0700

    Likes.test - With Likes - add `should render joke divs`

--- src/Components/Likes/Likes.test.js
+++ src/Components/Likes/Likes.test.js
@@ -5,6 +5,11 @@ import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from "react-router-dom";
 
+let jokes = [
+    'funny',
+];
+let mockedDislike = jest.fn();
+
 describe('No likes', () => {
     describe('Render', () => {
         it('should render an image and a header', () => {
@@ -18,4 +23,27 @@ describe('No likes', () => {
             expect(screen.getByText('You have no likes')).toBeInTheDocument();
         })
     })
+})
+describe('With likes', () => {
+    describe('Render', () => {
+        it('should render joke divs', () => {
+            render(
+                <BrowserRouter>
+                    <Likes 
+                        likes={jokes}
+                        dislike={mockedDislike}
+                        isLikes={true}
+                        decodeHtml={
+                            (param) => {
+                                return param;
+                            }
+                        }
+                    />
+                </BrowserRouter>
+            )
+            expect(screen.getByText('👎')).toBeInTheDocument();
+            expect(screen.getByText('funny')).toBeInTheDocument();
+        })
+    })
+
 })
