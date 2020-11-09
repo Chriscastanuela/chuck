0926f8428439f379ebed77349082c2589ad63b0b
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-08 18:06:48 -0700

    header.test

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -1,18 +1,18 @@
-// import { render, screen } from '@testing-library/react';
-// import Header from './Header';
-// import React from 'react'
-// import '@testing-library/jest-dom';
-// import userEvent from '@testing-library/user-event';
-// import { BrowserRouter } from "react-router-dom";
+import { render, screen } from '@testing-library/react';
+import Header from './Header';
+import React from 'react'
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from "react-router-dom";
 
 
-// it('1. should render a header', () => {
-// render(
-//     <BrowserRouter>
-//         <Header />
-//     </BrowserRouter>
-// );
+it('1. should render a header', () => {
+render(
+    <BrowserRouter>
+        <Header />
+    </BrowserRouter>
+);
 
-// expect(screen.getByText('Chuck Norris Jokes for the Bored')).toBeInTheDocument();
-// expect(screen.getByText('👍')).toBeInTheDocument();
-// });
+expect(screen.getByText('Chuck Norris Jokes for the Bored')).toBeInTheDocument();
+expect(screen.getByText('👍')).toBeInTheDocument();
+});
