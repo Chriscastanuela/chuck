8c0dfab6b2ab04b015c35403d6c5c3881d7eab63
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-08 18:06:33 -0700

    footer.test

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react'
 import '@testing-library/jest-dom';
-import userEvent from '@testing-library/user-event';
+
 
 let jokes = [
   {
