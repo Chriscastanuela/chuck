eb2ab6ece27ecef26718ad33fa35feb206202e7a
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-08 21:20:24 -0700

    add key to Main.js jokeDivs

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -4,6 +4,7 @@ import JokeDiv from '../JokeDiv/JokeDiv';
 import loading from '../../Assets/loading.png';
 
 export default function Main(props) {
+    let theKey = 0;
     if (props.jokes.length > 0) {
         let sortedJokes = props.jokes[0].value.sort((a, b) => {
             return a.joke.localeCompare(b.joke)
@@ -12,8 +13,10 @@ export default function Main(props) {
             <div className='main-section'>
                 {
                     sortedJokes.map(element => {
+                        theKey += 1;
                         return (
                             <JokeDiv 
+                            key={theKey}
                             decodeHtml={props.decodeHtml}
                             theJoke={element.joke}
                             like={props.like}
